57997164f14acef28ac8d4cb9360dee0e872ec44
qalisd <qaliblog9876@gmail.com>
2025-12-06 10:28:27 +0000

    fix: Update include path for llama.h
    - Change from 'llama.cpp/include/llama.h' to 'llama.h'
    - Include directories are already configured in CMakeLists.txt
    - This fixes the compilation error

--- app/src/main/cpp/llama_jni.cpp
+++ app/src/main/cpp/llama_jni.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <vector>
 #include <android/log.h>
-#include "llama.cpp/include/llama.h"
+#include "llama.h"
 
 #define LOG_TAG "LlamaJNI"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
